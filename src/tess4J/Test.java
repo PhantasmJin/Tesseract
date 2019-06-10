c9d527610de2c30c255f7441d6ad956c46d35e8c
PhantasmJin <33042654+PhantasmJin@users.noreply.github.com>
2019-06-10 10:50:50 -0700

    Update Test.java

--- src/tess4J/Test.java
+++ src/tess4J/Test.java
@@ -1,9 +1,7 @@
 package tess4J;
 
-import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.Robot;
-import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
@@ -50,7 +48,7 @@ public class Test { // NOTE: lib folder contains the jar files used in this proj
         	ImageIO.write(image, "png", new FileOutputStream("Choice3.png"));
 	}
 	
-	public static void getTess(File imageFile) throws IOException{	
+	public static void getTess(File imageFile) throws IOException{	 // get the ASCII format of the image
 		ITesseract instance = new Tesseract();
 		instance.setDatapath("D:\\My work\\MyEclipseProjects\\Tess4JOCR\\tessdata");
 		
@@ -73,9 +71,9 @@ public class Test { // NOTE: lib folder contains the jar files used in this proj
 		
 	}
 	
-	public static void getResultNumber(String c) throws IOException{
+	public static void getResultNumber(String c) throws IOException{ // web crawl
 	    String key="AIzaSyBoSXsswVlG9iwZ2hpMQticVv0MUCGBEdM";
-	    String qry = question + "-" + c;
+	    String qry = question;//question + "-" + c;
 	    URL url = new URL(
 	            "https://www.googleapis.com/customsearch/v1?key="+key+ "&cx=013036536707430787589:_pqjad5hr1a&q="+ qry + "&alt=json");
 	    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -83,45 +81,59 @@ public class Test { // NOTE: lib folder contains the jar files used in this proj
 	    conn.setRequestProperty("Accept", "application/json");
 	    BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
 
-	    String output;
+	    /*String output;
 	    String numOutput = "";
-	    //System.out.println("Output from Server .... \n");
 	    while ((output = br.readLine()) != null) {
-
+	    	//System.out.println(output);
 	        if(output.contains("\"totalResults\": \"")){                
 	            numOutput = output.substring(output.indexOf("\"totalResults\": \"")+("\"totalResults\": \"").length(), output.indexOf("\","));
-	        }     
+	        }
 	    }
-	    //System.out.println(numOutput);
 	    if (counter == 2)
 			num1 = Integer.parseInt(numOutput);
 		else if (counter == 3)
 			num2 = Integer.parseInt(numOutput);
 		else
-			num3 = Integer.parseInt(numOutput);
+			num3 = Integer.parseInt(numOutput);*/
+	    
+	    String output;
+	    int i = 0;
+	    while((output = br.readLine()) != null) {
+	    	System.out.println(output);
+	    	if (output.toLowerCase().contains(c.toLowerCase())) {
+	    		i++;
+	    	}
+	    }
+		if (counter == 2)
+			num1 = i;
+		else if (counter == 3)
+			num2 = i;
+		else
+			num3 = i;
 	    conn.disconnect();  
 	}
 	
 	public static void main(String[] args) throws AWTException, IOException{
-		takePhoto(30, 270, 490, 160); //CS: 30, 270, 490, 170 //HQ: 30, 270, 490, 160
+		takePhoto(200, 220, 550, 140); //CS: 30, 270, 490, 170 //HQ: 30, 270, 490, 160
         getTess(questionImage);
         
-        takePhoto(65, 490, 400, 70); //CS: 65, 475, 400, 75 //HQ: 65, 460, 400, 70
+        takePhoto(220, 370, 400, 80); //CS: 65, 475, 400, 75 //HQ: 65, 460, 400, 70
         getTess(choice1Image);
         getResultNumber(choice1);
         
-        takePhoto(65, 580, 400, 70); //CS: 65, 575, 400, 75 //HQ: 65, 560, 400, 70
+        takePhoto(220, 490, 400, 80); //CS: 65, 575, 400, 75 //HQ: 65, 560, 400, 70
         getTess(choice2Image);
         getResultNumber(choice2);
         
-        takePhoto(65, 660, 400, 70); //CS: 65, 680, 400,75 //HQ: 65, 660, 400, 70
+        takePhoto(220, 570, 400, 80); //CS: 65, 680, 400,75 //HQ: 65, 660, 400, 70
         getTess(choice3Image);
         getResultNumber(choice3);
         
-        System.out.println(num1 + "\n" + num2 + "\n" + num3);
+        System.out.println("-------------------");
+        System.out.println(choice1 + ": " + num1 + "\n" + choice2 + ": " + num2 + "\n" + choice3 + ": " +  num3); // prints the number of search results for each answer choice.
         System.out.println("-------------------");
         
-        if(num1 > num2 && num1 > num3) System.out.println(1);
+        if(num1 > num2 && num1 > num3) System.out.println(1); // prints the highest search result.
         else if (num2 > num3 && num2 > num1) System.out.println(2);
         else System.out.println(3);
 	}
